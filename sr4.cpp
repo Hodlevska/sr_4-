dff2d2d32ab0602b8cd089e4838f5a1216e4bdb5
Hodlevska <73657261+Hodlevska@users.noreply.github.com>
2024-11-01 12:00:24 +0000

    First version

--- sr4.cpp
+++ sr4.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double y, x; 
+	cout << "Введiть значення х= ";
+	cin >> x;
+	return 0;
+}
+
 
